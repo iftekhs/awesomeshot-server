fb77224fd0236b003effe66d7dc1ddce2187b287
iftekhs <iftekhstudio@gmail.com>
2022-11-09 17:15:30 +0600

    installed dotenv

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
+require('dotenv').config();
+
 // middlewares
 app.use(cors());
 app.use(express.json());
@@ -12,6 +14,22 @@ app.use(express.json());
 const main = async () => {
   const uri = 'mongodb://localhost:27017';
   const client = new MongoClient(uri);
+
+  function veryifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        res.status(403).send({ message: 'unauthorized access' });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
+
   try {
     const servicesCollection = client.db('awesomeshot').collection('services');
     app.get('/services', async (req, res) => {
@@ -30,6 +48,12 @@ const main = async () => {
       const id = req.params.id;
       const service = await servicesCollection.findOne({ _id: ObjectId(id) });
       res.send(service);
+    });
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
     });
   } catch (error) {
     console.log(error);
